709581ee13a53aecbd57e798f90591c74a21207b
timproctor <timandcamiproctor@gmail.com>
2014-06-10 09:37:28 -0600

    Implement 'Idea'

---
+++ README.md
@@ -0,0 +1,2 @@
+Rank ideas that have a title and description. The ideas can be
+liked and from the liking, a rank is obtained.
---
+++ test/ideabox/idea_test.rb
@@ -0,0 +1,36 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ideabox/idea'
+
+class IdeaTest < Minitest::Test
+  def test_basic_idea
+    idea = Idea.new("title", "description")
+    assert_equal "title", idea.title
+    assert_equal "description", idea.description
+  end
+
+  def test_ideas_can_be_liked
+    idea = Idea.new("diet", "carrots and cucumbers")
+    assert_equal 0, idea.rank
+    idea.like!
+    assert_equal 1, idea.rank
+    idea.like!
+    assert_equal 2, idea.rank
+  end
+
+  def test_ideas_can_be_sorted_by_rank
+    diet = Idea.new("diet", "cabbage soup")
+    exercise = Idea.new("exercise", "long distance running")
+    drink = Idea.new("drink", "carrot smoothy")
+
+    exercise.like!
+    exercise.like!
+    drink.like!
+
+    ideas = [drink, exercise, diet]
+
+    assert_equal [diet, drink, exercise], ideas.sort
+  end
+
+end
